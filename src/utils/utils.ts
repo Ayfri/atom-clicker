f0535beb4322c735462f4df0728605c79cdbbd9b
Ayfri <pierre.ayfri@gmail.com>
2021-02-13 14:22:56 +0100

    Added sleep util function.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,7 @@
 export function getTextureByName(name: string): PIXI.Texture | undefined {
 	return PIXI.Loader.shared.resources[name].texture;
 }
+
+export function sleep(ms: number): Promise<unknown> {
+	return new Promise( resolve => setTimeout(resolve, ms) );
+}
