dd9eaf6bd270e8bcab1538559ab15837d50d5b2b
Ayfri <pierre.ayfri@gmail.com>
2021-02-13 02:32:05 +0100

    Prettified code.

--- src/client/MouseManager.ts
+++ src/client/MouseManager.ts
@@ -5,23 +5,23 @@ export type ButtonEvent = 'down' | 'up' | 'dblclick';
 
 export class Button extends PIXI.utils.EventEmitter {
 	private _pressedAt: number = 0;
-	
+
 	constructor(public readonly id: number) {
 		super();
 		buttons.add(this);
 	}
-	
+
 	private _isPressed: boolean = false;
-	
+
 	get isPressed(): boolean {
 		return this._isPressed;
 	}
-	
+
 	get duration(): number {
 		if (!this._isPressed) return 0;
 		return Date.now() - this._pressedAt;
 	}
-	
+
 	handle(event: MouseEvent, action: ButtonEvent) {
 		this.emit(action, event, this.duration);
 		if (action === 'down') {
@@ -31,17 +31,17 @@ export class Button extends PIXI.utils.EventEmitter {
 			this._isPressed = false;
 		}
 	}
-	
+
 	on(event: ButtonEvent, fn: (event?: KeyboardEvent, duration?: number) => unknown, context?: any): this {
 		super.on(event, fn, context);
 		return this;
 	}
-	
+
 	once(event: ButtonEvent, fn: (eventN: KeyboardEvent, durationN: number) => unknown, context?: any): this {
 		super.once(event, fn, context);
 		return this;
 	}
-	
+
 	isMineEvent(event: MouseEvent): boolean {
 		return event.button === this.id;
 	}
@@ -53,7 +53,7 @@ export let mousePosition: PIXI.Point = new PIXI.Point();
 
 document.addEventListener('mousemove', (event: MouseEvent) => {
 	mousePosition.set(event.x, event.y);
-})
+});
 
 document.addEventListener('mouseup', (event: MouseEvent) => {
 	buttons.forEach(button => {
--- src/utils/EventEmitter.ts
+++ src/utils/EventEmitter.ts
@@ -4,11 +4,11 @@ export default class EventEmitter<Events extends Record<string, any[]>> extends 
 	public emit<K extends keyof Events>(event: string | symbol, ...args: Events[K]): boolean {
 		return super.emit(event, ...args);
 	}
-	
+
 	public on<K extends string & keyof Events>(event: K, fn: (...params: Events[K]) => void, context?: any) {
 		return super.on(event, fn, context);
 	}
-	
+
 	public once<K extends string & keyof Events>(event: K, fn: (...params: Events[K]) => void, context?: any) {
 		return super.once(event, fn, context);
 	}
