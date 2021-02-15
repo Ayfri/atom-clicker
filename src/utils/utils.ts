447b8d074262caf42e3ba5612387851a35c9c12d
Ayfri <pierre.ayfri@gmail.com>
2021-02-15 02:56:53 +0100

    Added deepCopy util function.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,6 +2,10 @@ export function getTextureByName(name: string): PIXI.Texture | undefined {
 	return PIXI.Loader.shared.resources[name].texture;
 }
 
+export function deepCopy<T>(source: T): T {
+	return JSON.parse(JSON.stringify(source));
+}
+
 export function sleep(ms: number): Promise<unknown> {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
