485213539f5843b31c99c2c1f927020414e0352e
Ayfri <pierre.ayfri@gmail.com>
2021-02-15 04:02:27 +0100

    Added default properties to Buyables.

--- src/buyables/Buyable.ts
+++ src/buyables/Buyable.ts
@@ -1,4 +1,10 @@
+import Overlay from '../elements/Overlay.js';
+
 export interface Buyable {
 	readonly price: number;
 	readonly canBeBought: boolean;
+	container: PIXI.Container;
+	overlay: Overlay;
+	priceText?: PIXI.Text;
+	nameText?: PIXI.Text;
 }
