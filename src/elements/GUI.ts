8e7dd4f050e35c10e3daba185c5c83dad4b4c5ff
Ayfri <pierre.ayfri@gmail.com>
2021-02-15 03:36:04 +0100

    Prettified code.

--- src/elements/GUI.ts
+++ src/elements/GUI.ts
@@ -8,10 +8,10 @@ export default class GUI {
 	public container: PIXI.Container;
 	public clicksTexts: PIXI.Text[] = [];
 	public CPSText: PIXI.Text;
-	
+
 	public constructor() {
 		this.container = new PIXI.Container();
-		
+
 		const style = new PIXI.TextStyle({
 			dropShadow: true,
 			dropShadowBlur: 5,
@@ -20,24 +20,24 @@ export default class GUI {
 			fontSize: 60,
 			padding: 20,
 		});
-		
+
 		this.atomsCountText = new PIXI.Text('', style);
 		this.atomsCountText.anchor.set(0.5);
 		this.atomsCountText.position.set(window.innerWidth / 2, 40);
-		
+
 		this.APSText = new PIXI.Text('', JSON.parse(JSON.stringify(style)));
 		this.APSText.style.fontSize = 35;
 		this.APSText.anchor.set(0.5);
 		this.APSText.position.set(window.innerWidth / 2, 80);
-		
+
 		this.atomsPerClicksText = new PIXI.Text('');
 		this.atomsPerClicksText.anchor.set(0, 0.5);
 		this.atomsPerClicksText.position.set(window.innerWidth / 40, window.innerHeight / 15);
-		
+
 		this.CPSText = new PIXI.Text('');
 		this.CPSText.anchor.set(0, 0.5);
 		this.CPSText.position.set(window.innerWidth / 40, window.innerHeight / 15 + 30);
-		
+
 		for (let i = 0; i < 50; i++) {
 			const text: PIXI.Text = new PIXI.Text('');
 			text.visible = false;
@@ -45,51 +45,42 @@ export default class GUI {
 			text.anchor.set(0.5);
 			this.clicksTexts.push(text);
 		}
-		
-		this.container.addChild(
-			this.atomsCountText,
-			this.APSText,
-			this.atomsPerClicksText,
-			this.CPSText,
-			...this.clicksTexts
-		);
+
+		this.container.addChild(this.atomsCountText, this.APSText, this.atomsPerClicksText, this.CPSText, ...this.clicksTexts);
 	}
-	
+
 	public click(position: PIXI.Point) {
-		const text = this.clicksTexts.find((text) => !text.visible);
+		const text = this.clicksTexts.find(text => !text.visible);
 		if (!text) return;
 		text.text = `+${game.totalAtomsPerClicks}`;
 		text.alpha = 1;
 		text.visible = true;
 		text.position = position;
 		text.x += Math.random() * 10 - 5;
-		setTimeout(() => text.visible = false, 1000);
+		setTimeout(() => (text.visible = false), 1000);
 	}
-	
+
 	public update() {
 		this.atomsCountText.text = `${game.atomsCount.toString().split('.')[0]} atoms`;
 		this.atomsCountText.position.x = window.innerWidth / 2;
-		
-		this.APSText.text = `per second: ${
-			game.atomsPerSecond
-				.add(game.atomsPerClicks.mul(this.clicksPerSeconds))
-				.toString()
-				.replace(/(\d+\.\d{2})\d+/g, '$1')
-		}`;
+
+		this.APSText.text = `per second: ${game.totalAtomsPerSecond.toString().replace(/(\d+\.\d{2})\d+/g, '$1')}`;
 		this.APSText.position.x = window.innerWidth / 2;
-		
+
 		this.atomsPerClicksText.text = `Atoms per clicks: ${game.totalAtomsPerClicks.toString()}`;
 		this.atomsPerClicksText.position.set(window.innerWidth / 40, window.innerHeight / 15);
-		
+
 		this.CPSText.text = `Clicks per second: ${this.clicksPerSeconds}`;
 		this.CPSText.position.set(window.innerWidth / 40, window.innerHeight / 15 + 30);
-		
-		this.clicksTexts.filter(text => text.visible).forEach((text) => {
-			text.position.y--;
-			text.alpha -= 1/PIXI.Ticker.shared.FPS;
-		});
+
+		this.clicksTexts
+			.filter(text => text.visible)
+			.forEach(text => {
+				text.position.y--;
+				text.alpha -= 1 / PIXI.Ticker.shared.FPS;
+			});
 	}
-	
+
 	public get clicksPerSeconds(): number {
 		return this.clicksTexts.filter(text => text.visible).length;
 	}
--- src/elements/Game.ts
+++ src/elements/Game.ts
@@ -1,10 +1,10 @@
-import {BigFloat} from 'bigfloat.js';
+import { BigFloat } from 'bigfloat.js';
 import * as PIXI from 'pixi.js';
-import {app} from '../app.js';
+import { app } from '../app.js';
 import buildings from '../assets/buildings.json';
 import upgrades from '../assets/upgrades.json';
-import Building, {BuildingOptions} from '../buyables/Building.js';
-import Upgrade, {ConditionType, UpgradeType} from '../buyables/Upgrade.js';
+import Building, { BuildingOptions } from '../buyables/Building.js';
+import Upgrade, { ConditionType, UpgradeType } from '../buyables/Upgrade.js';
 import Clickable from './Clickable.js';
 import GUI from './GUI.js';
 
@@ -21,119 +21,141 @@ export default class Game {
 	public totalClicks: number = 0;
 	public upgrades: Upgrade<UpgradeType, ConditionType>[] = [];
 	public gui: GUI;
-	
+
 	public constructor() {
 		this.gui = new GUI();
 		app.stage.addChild(this.gui.container);
-		
+
 		this.mainAtom = new Clickable(PIXI.Texture.WHITE);
 		this.mainAtom.sprite.height = 400;
 		this.mainAtom.sprite.width = 400;
 		this.mainAtom.sprite.zIndex = -100;
 		this.mainAtom.sprite.position.set(window.innerWidth / 2 - this.mainAtom.sprite.width / 2, 150);
 		app.stage.addChild(this.mainAtom.sprite);
-		
+
 		this.mainAtom.on('click', async (_, position) => {
 			this.atomsCount = this.atomsCount.add(this.totalAtomsPerClicks);
 			this.totalAtomsProduced = this.totalAtomsProduced.add(this.totalAtomsPerClicks);
 			this.totalClicks++;
 			this.gui.click(position);
 		});
-		
+
 		buildings.forEach((building: BuildingOptions) => this.buildings.push(new Building(building)));
-		
+
 		upgrades.forEach((upgrade: any) =>
 			this.upgrades.push(
 				new Upgrade(
 					{
 						name: upgrade.name,
 						description: upgrade.description,
-						price: upgrade.price
+						price: upgrade.price,
 					},
 					upgrade.effect,
 					upgrade.condition
 				)
 			)
 		);
-		
-		this.buildings.forEach((building) => {
+
+		this.buildings.forEach(building => {
 			const levels = [15, 25, 50, 100, 200, 300, 400, 500, 750, 1000, 1500, 2000];
-			
+
 			for (let level of levels) {
-				this.upgrades.push(new Upgrade({
-					name: `${level} ${building.name}.`,
-					description: `Buy ${level} ${building.name}.`,
-					price: Math.round(building.startingPrice * building.priceMultiplier ** level * Math.log(level * 10))
-				}, {
-					kind: 'building',
-					building: building.name,
-					multiplier: level / 10
-				}, {
-					kind: 'building',
-					building: building.name,
-					count: level
-				}));
+				this.upgrades.push(
+					new Upgrade(
+						{
+							name: `${level} ${building.name}.`,
+							description: `Buy ${level} ${building.name}.`,
+							price: Math.round(building.startingPrice * building.priceMultiplier ** level * Math.log(level * 10)),
+						},
+						{
+							kind: 'building',
+							building: building.name,
+							multiplier: level / 10,
+						},
+						{
+							kind: 'building',
+							building: building.name,
+							count: level,
+						}
+					)
+				);
 			}
 		});
-		
+
 		for (let level = 100; level < 10e7; level *= 10) {
-			this.upgrades.push(new Upgrade({
-				name: `${level} clicks !`,
-				description: `You have clicked ${level} times.`,
-				price: level * 10
-			}, {
-				kind: 'click',
-				multiplier: Math.log10(level)
-			}, {
-				kind: 'click',
-				count: level
-			}));
+			this.upgrades.push(
+				new Upgrade(
+					{
+						name: `${level} clicks !`,
+						description: `You have clicked ${level} times.`,
+						price: level * 10,
+					},
+					{
+						kind: 'click',
+						multiplier: Math.log10(level),
+					},
+					{
+						kind: 'click',
+						count: level,
+					}
+				)
+			);
 		}
-		
-		for(let level = 1000; level < 10e20; level *= 10) {
-				this.upgrades.push(new Upgrade({
-					name: `${level} atoms generated.`,
-					description: `You have generated ${level} total atoms.`,
-					price: level * 10
-				}, {
-					kind: 'aps',
-					multiplier: Math.log10(level)
-				}, {
-					kind: 'atoms',
-					count: level
-				}));
+
+		for (let level = 1000; level < 10e20; level *= 10) {
+			this.upgrades.push(
+				new Upgrade(
+					{
+						name: `${level} atoms generated.`,
+						description: `You have generated ${level} total atoms.`,
+						price: level * 10,
+					},
+					{
+						kind: 'aps',
+						multiplier: Math.log10(level),
+					},
+					{
+						kind: 'atoms',
+						count: level,
+					}
+				)
+			);
 		}
-		
+
 		this.buildings.forEach(buildings => app.stage.addChild(buildings.container));
 		this.upgrades.forEach(upgrade => app.stage.addChild(upgrade.container));
 	}
-	
+
 	get totalAtomsPerClicks(): BigFloat {
 		return this.atomsPerClicks.add(this.atomsPerSecond.mul(this.atomsPerClicksAPSBoost)).mul(100).ceil().div(100);
 	}
-	
+
+	get totalAtomsPerSecond(): BigFloat {
+		return this.atomsPerSecond.add(this.atomsPerClicks.mul(this.gui.clicksPerSeconds));
+	}
+
 	public update() {
 		app.stage.sortChildren();
 		this.gui.update();
 		this.mainAtom.sprite.position.x = window.innerWidth / 2 - this.mainAtom.sprite.width / 2;
-		
+
 		this.buildings.forEach((building, index) => {
 			building.update();
 			building.container.x = window.innerWidth - building.container.width;
 			building.container.y = index * (building.container.height + 5) + window.innerHeight / 4;
 		});
-		
+
 		for (const upgrade of this.upgrades.sort((u1, u2) => u1.price - u2.price)) {
 			const index = this.upgrades.filter(upgrade => upgrade.unlocked && !upgrade.owned).indexOf(upgrade);
 			upgrade.update();
 			upgrade.container.y = index * (upgrade.container.height + 5) + window.innerHeight / 4;
 			upgrade.container.visible = upgrade.unlocked && !upgrade.owned;
 		}
-		
+
 		this.atomsCount = this.atomsCount.add(this.atomsPerSecond.dividedBy(PIXI.Ticker.shared.FPS));
 		this.totalAtomsProduced = this.totalAtomsProduced.add(this.atomsPerSecond.dividedBy(PIXI.Ticker.shared.FPS));
 	}
-	
+
 	public calculateAPS() {
 		this.atomsPerSecond = new BigFloat(
 			this.buildings.map(building => building.totalAtomPerSecond * this.buildingsGlobalBoost).reduce((previous, current) => previous + current)
