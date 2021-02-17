094f312edbd7303e9dfeec4c3c93dba4504588c2
Ayfri <pierre.ayfri@gmail.com>
2021-02-17 15:57:00 +0100

    Removed overloads from Clickable class.

--- src/components/Clickable.ts
+++ src/components/Clickable.ts
@@ -13,8 +13,6 @@ export type ClickableEvents = {
 export default class Clickable extends EventEmitter<ClickableEvents> {
 	public sprite: PIXI.Sprite;
 
-	public constructor(textureName: string);
-	public constructor(texture: PIXI.Texture);
 	public constructor(texture: PIXI.Texture | string) {
 		super();
 
